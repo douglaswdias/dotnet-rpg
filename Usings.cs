87e009da248797737077f0e17ebd36b813b342bf
Douglas Willian Dias <douglas.wdias@hotmail.com>
2024-05-16 16:02:09 -0300

    feat: add entity framework, sql server and implement the DataContext

--- Usings.cs
+++ Usings.cs
@@ -3,3 +3,4 @@ global using dotnet_rpg.Services.CharacterService;
 global using Microsoft.AspNetCore.Mvc;
 global using dotnet_rpg.Dtos.Character;
 global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
